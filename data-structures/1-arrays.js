89df0e54aed33aed306f3189ab6347e4fe2b086d
Drakkarrr <junrey404@gmail.com>
2023-11-20 09:53:41 +0800

    reduced and sort

--- data-structures/1-arrays.js
+++ data-structures/1-arrays.js
@@ -3,7 +3,58 @@ arr.push('drakkar'); // add element at the end
 arr.unshift(0); // add element in the beginning
 arr.pop(); // remove element at the end
 arr.shift(); // remove element in the beginning
-for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
-    var item = arr_1[_i];
-    console.log(item);
-}
+// for (const item of arr) console.log(item);
+var mapped = arr.map(function (item) {
+    if (typeof item === 'number')
+        return item * 2;
+});
+// console.log(mapped);
+var filtered = arr.filter(function (item) {
+    if (typeof item === 'number')
+        return item > 2;
+});
+// console.log(filtered);
+var pilots = [
+    {
+        id: 1,
+        name: 'Drakkar',
+        years: 14,
+    },
+    {
+        id: 2,
+        name: 'Junrey',
+        years: 30,
+    },
+    {
+        id: 3,
+        name: 'Lañas',
+        years: 16,
+    },
+    {
+        id: 4,
+        name: 'Evangelist',
+        years: 22,
+    },
+];
+var totalYears = pilots.reduce(function (acc, pilot) { return acc + pilot.years; }, 0);
+// console.log(totalYears);
+var pilotNames = pilots.reduce(function (acc, pilot) {
+    return acc + pilot.name + ' ';
+}, '');
+// console.log(pilotNames);
+var sortedNames = pilots.sort(function (a, b) {
+    if (a.name > b.name)
+        return 1;
+    else if (a.name < b.name)
+        return -1;
+    return 0;
+});
+console.log(sortedNames);
+var sortedYears = pilots.sort(function (a, b) {
+    if (a.years > b.years)
+        return 1;
+    else if (a.years < b.years)
+        return -1;
+    return 0;
+});
+console.log(sortedYears);
--- data-structures/1-arrays.ts
+++ data-structures/1-arrays.ts
@@ -4,8 +4,64 @@ arr.unshift(0); // add element in the beginning
 arr.pop(); // remove element at the end
 arr.shift(); // remove element in the beginning
 
-for (const item of arr) console.log(item);
+// for (const item of arr) console.log(item);
 
 const mapped = arr.map((item) => {
   if (typeof item === 'number') return item * 2;
 });
+
+// console.log(mapped);
+
+const filtered = arr.filter((item) => {
+  if (typeof item === 'number') return item > 2;
+});
+
+// console.log(filtered);
+
+const pilots = [
+  {
+    id: 1,
+    name: 'Drakkar',
+    years: 14,
+  },
+  {
+    id: 2,
+    name: 'Junrey',
+    years: 30,
+  },
+  {
+    id: 3,
+    name: 'Lañas',
+    years: 16,
+  },
+  {
+    id: 4,
+    name: 'Evangelist',
+    years: 22,
+  },
+];
+
+const totalYears = pilots.reduce((acc, pilot) => acc + pilot.years, 0);
+// console.log(totalYears);
+
+const pilotNames = pilots.reduce((acc, pilot) => {
+  return acc + pilot.name + ' ';
+}, '');
+
+// console.log(pilotNames);
+
+const sortedNames = pilots.sort((a, b) => {
+  if (a.name > b.name) return 1;
+  else if (a.name < b.name) return -1;
+  return 0;
+});
+
+console.log(sortedNames);
+
+const sortedYears = pilots.sort((a, b) => {
+  if (a.years > b.years) return 1;
+  else if (a.years < b.years) return -1;
+  return 0;
+});
+
+console.log(sortedYears);
