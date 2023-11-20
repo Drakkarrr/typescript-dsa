7925d790de3706ed906714a273967aa4314923a8
Drakkarrr <junrey404@gmail.com>
2023-11-20 10:05:03 +0800

    function on objects

--- data-structures/2-objects.js
+++ data-structures/2-objects.js
@@ -2,6 +2,12 @@ var obj = {
     name: 'Drakkar',
     age: 22,
     isCool: true,
+    sayPresent: function () { return console.log("".concat(obj.name, " is present!")); },
+    sayAbsent: function () {
+        console.log("".concat(this.name, " is absent!"));
+    },
 };
 console.log(obj.name);
 console.log(obj['age']);
+obj.sayPresent();
+obj.sayAbsent();
--- data-structures/2-objects.ts
+++ data-structures/2-objects.ts
@@ -2,7 +2,13 @@ const obj = {
   name: 'Drakkar',
   age: 22,
   isCool: true,
+  sayPresent: (): void => console.log(`${obj.name} is present!`),
+  sayAbsent: function (): void {
+    console.log(`${this.name} is absent!`);
+  },
 } as const;
 
 console.log(obj.name);
 console.log(obj['age']);
+obj.sayPresent();
+obj.sayAbsent();
