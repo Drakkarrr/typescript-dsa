c20e7407b76169b4ad19faff41d418c2ee4bbe2d
Drakkarrr <junrey404@gmail.com>
2023-11-26 17:33:16 +0800

    recursive binary search algo

---
+++ algorithm/searching/recursive-binary-search.ts
@@ -0,0 +1,29 @@
+function recursiveBinarySearch(arr: number[], target: number): number {
+  return search(arr, target, 0, arr.length - 1);
+}
+
+function search(
+  arr: number[],
+  target: number,
+  leftIndex: number,
+  rightIndex: number
+): number {
+  if (leftIndex > rightIndex) {
+    return -1;
+  }
+
+  let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+  if (target === arr[middleIndex]) {
+    return middleIndex;
+  }
+
+  if (target < arr[middleIndex]) {
+    return search(arr, target, leftIndex, middleIndex - 1);
+  } else {
+    return search(arr, target, middleIndex + 1, rightIndex);
+  }
+}
+
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 10)); // 4
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 6)); // 3
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 20)); // -1
